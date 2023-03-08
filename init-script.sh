4a630a4bad3edd1a4b3a7150b9e6a672cd331a03
Peter <petnguyen@axon.com>
2023-03-08 16:18:42 +0700

    feat: add second lab with tf

--- init-script.sh
+++ init-script.sh
@@ -2,9 +2,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
-yum update -y
-yum -y remove httpd
-yum -y remove httpd-tools
+# yum update -y
+# yum -y remove httpd
+# yum -y remove httpd-tools
 yum install -y httpd24 php72 mysql57-server php72-mysqlnd
 service httpd start
 chkconfig httpd on
--- terraform.tf
+++ terraform.tf
@@ -21,6 +21,4 @@ terraform {
       source = "hashicorp/random"
     }
   }
-
-  required_version = "~> 1.2.0"
 }
